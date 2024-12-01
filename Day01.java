32e7dc42649e26cbe36a3599ba11cca424bf0613
UniK <unik@users.noreply.github.com>
2024-12-01 10:53:42 +0100

    solve day one, part one

---
+++ Day01.java
@@ -0,0 +1,73 @@
+///usr/bin/env jbang "$0" "$@" ; exit $?
+//JAVA 21
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static java.lang.System.*;
+
+class Day01 {
+    public static void main(String... args) throws IOException {
+
+        if (args.length != 1) {
+            err.println("Usage: Day01.java <path-to-input-file>");
+            exit(1);
+        }
+
+        out.println("--- Day 1: Historian Hysteria ---");
+
+        Path path = Paths.get(args[0]);
+
+        List<Integer> listLeft;
+        List<Integer> listRight;
+
+/*
+        for (String line : collect) {
+            String[] pairs = line.split("\\s+");
+            if (pairs.length == 2) {
+                listLeft.add(Integer.parseInt(pairs[0]));
+                listRight.add(Integer.parseInt(pairs[1]));
+            }
+        }
+*/
+        try (Stream<String> lines = Files.lines(path)) {
+            var lists = lines.map(line -> line.split("\\s+"))
+                    .filter(pairs -> pairs.length == 2)
+                    .map(pairs -> List.of(
+                            Integer.parseInt(pairs[0]),
+                            Integer.parseInt(pairs[1])
+                    ))
+                    .collect(
+                            Collectors.teeing(
+                                    Collectors.mapping(p -> p.get(0), Collectors.toList()),
+                                    Collectors.mapping(p -> p.get(1), Collectors.toList()),
+                                    List::of
+                            )
+                    );
+
+            listLeft = lists.get(0).stream().sorted().toList();
+            listRight = lists.get(1).stream().sorted().toList();
+        }
+
+        List<Integer> differences = IntStream
+                .range(0, Math.min(listLeft.size(), listRight.size()))
+                .mapToObj(i -> Math.abs(listLeft.get(i) - listRight.get(i)))
+                .toList();
+
+        int sumOfDifferences = differences.stream().mapToInt(Integer::intValue).sum();
+
+        out.println("--- Part One ---");
+        out.println("Sum of differences: " + sumOfDifferences);
+
+
+
+        out.println("--- Part Two ---");
+        out.println("Sum of products: ");
+    }
+}
